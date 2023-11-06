705e3af42bc6970d87816781eeb843b905773c2c
HarshBardolia01 <harshbardolia02@gmail.com>
2023-11-06 17:33:58 +0530

    created transaction services and controller

---
+++ backend/controller/transaction-controller.js
@@ -0,0 +1,3 @@
+import * as transactionService from "../services/transaction-service.js";
+import * as userService from "../services/user-service.js";
+import moment from "moment";
---
+++ backend/services/transaction-service.js
@@ -0,0 +1,33 @@
+import Transaction from "../models/transactionSchema.js";
+
+export const createTransaction = async (request) => {
+    const { title, amount, category, description, transactionType, date, user } = request;
+    const transaction = await Transaction.create({
+        title,
+        amount,
+        category,
+        description,
+        transactionType,
+        date,
+        user
+    });
+    if (!transaction) return null;
+    return transaction;
+}
+
+export const getAllTransaction = async (query) => {
+    return await Transaction.find(query);
+}
+
+export const getTransactionById = async (id) => {
+    const transaction = await Transaction.findById(id);
+    if (!transaction) return null;
+    return transaction;
+}
+
+export const deleteTransactionById = async (id) => {
+    const transaction = await Transaction.findById(id);
+    if (!transaction) return false;
+    await transaction.deleteOne();
+    return true;
+}
